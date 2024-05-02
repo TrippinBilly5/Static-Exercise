d324915754787bcdaf987f85d466c7ff9b5b6251
mark aldridge <mark.aldridge1@protonmail.com>
2024-05-02 15:13:40 -0700

    Static excercises

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Put in a temp in Fahrenheit and I'll convert it to Celsius:");
+            double fTemp = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{fTemp} F is equal to {TempConverter.FahrenheitToCelsius(fTemp)} C");
+            Console.WriteLine();
+            Console.WriteLine("Put in a temp in Celsius and I'll convert it to Fahrenheit:");
+            double cTemp = double.Parse(Console.ReadLine());
+            Console.WriteLine($"{cTemp} C is equal to {TempConverter.CelsiusToFahrenheit(cTemp)} F");
         }
     }
 }
---
+++ StaticExercise/TempConverter.cs
@@ -0,0 +1,14 @@
+﻿namespace StaticExercise
+{
+    public static class TempConverter
+    {
+        public static double FahrenheitToCelsius(double tempF)
+        {
+            return (tempF - 32) / 1.8;
+        }
+        public static double CelsiusToFahrenheit(double tempC)
+        {
+            return (tempC * 1.8) + 32;
+        }
+    }
+}
